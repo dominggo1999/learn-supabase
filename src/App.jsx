4dad65f855fa87aa9f45df75d23bbc3d647d9a01
dominggo1999 <arnolddominggo19@gmail.com>
2021-09-24 23:11:10 +0700

    Connect To Supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,19 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
 
-function App() {
-  const [count, setCount] = useState(0)
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+const App = () => {
+  console.log(supabase);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      hello bosku
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.json
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import macrosPlugin from 'vite-plugin-babel-macros';
+import envCompatible from 'vite-plugin-env-compatible';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    reactRefresh(),
+    macrosPlugin(),
+    envCompatible({
+      prefix: 'REACT_APP',
+    }),
+  ],
+  define: {
+    'process.platform': JSON.stringify('win32'),
+    'process.env': {},
+  },
+});
